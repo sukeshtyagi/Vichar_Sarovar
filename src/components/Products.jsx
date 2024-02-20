e96af9915a4e6ddf74c3ff950272b3df564da048
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-20 14:46:02 +0530

    Products Container Created

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import style from "../style/Product.module.css";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <div className={style.productsOuterContainer}>
+      <div className={style.productContainer}>
+        <p>
+          Rating <span>Update to be done</span>
+        </p>
+        <div className={style.productDetailsContainer}>
+          <div className={style.productImage}>
+            <img src="" alt="image of product" />
+          </div>
+          <div className={style.productDescription}>
+            <p>
+              <span>Show name here</span>
+            </p>
+            <p>
+              <span>Show price here</span>
+            </p>
+            <p>
+              <span>Show author here</span>
+            </p>
+            <p>
+              <span>Show category here</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
