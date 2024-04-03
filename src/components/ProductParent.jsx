603e9ddc0a1e00b51b163d87082d34b871d17bb8
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-04-03 07:00:29 +0530

    Add Product component code refactored and css updated

--- src/components/ProductParent.jsx
+++ src/components/ProductParent.jsx
@@ -49,45 +49,57 @@ function ProductParent({
     <div className={style.newProductContainer}>
       <h2>{containerHeading}</h2>
       <form onSubmit={handleDataUpload}>
-        <div className={style.newProductDetails}>
-          <div className={style.newProductLabels}>
-            <label>Name</label>
-            <label>Image url</label>
-            <label>Author</label>
-            <label>Price</label>
-            <label>Category</label>
-          </div>
-
-          <div className={style.newProductInputs}>
+        
+        <div className={style.allDetails}>
+         
+          <div className={style.individualDetail}>
+            <label className={style.inidividualDetailLabel}>Name</label>
             <input
+              className={style.detailInput}
               type="text"
               name="name"
               value={productData.name}
               onChange={handleChange}
             />
+          </div>
 
+          <div className={style.individualDetail}>
+            <label className={style.inidividualDetailLabel}>Image url</label>
             <input
+              className={style.detailInput}
               type="text"
               name="image"
               value={productData.image}
               onChange={handleChange}
             />
+          </div>
 
+          <div className={style.individualDetail}>
+            <label className={style.inidividualDetailLabel}>Author</label>
             <input
+              className={style.detailInput}
               type="text"
               name="author"
               value={productData.author}
               onChange={handleChange}
             />
+          </div>
 
+          <div className={style.individualDetail}>
+            <label className={style.inidividualDetailLabel}>Price</label>
             <input
+              className={style.detailInput}
               type="number"
               name="price"
               value={productData.price}
               onChange={handleChange}
-            />
+            />{" "}
+          </div>
 
+          <div className={style.individualDetail}>
+            <label className={style.inidividualDetailLabel}>Category</label>
             <input
+              className={style.detailInput}
               type="text"
               name="category"
               value={productData.category}
@@ -96,7 +108,12 @@ function ProductParent({
           </div>
         </div>
         {uploadButton && (
-          <button onClick={handleDataUploadAndNavigate}>{uploadButton}</button>
+          <button
+            className={style.newProductContainerButton}
+            onClick={handleDataUploadAndNavigate}
+          >
+            {uploadButton}
+          </button>
         )}
 
         {updateButton && (
